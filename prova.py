ae7755e0d333af1380c733f47d08cfc6c604493b
RafaelaLuciana <rafaelaluciana.neves@gmail.com>
2024-09-26 20:17:28 -0300

    Add files via upload

---
+++ prova.py
@@ -0,0 +1,10 @@
+#Programa para calcular a área de um retângulo
+
+print("Olá! Vamo descobrir a área do seu retângulo! ")
+
+base = int(input(" Insira a base do rentângulo:  "))
+altura = int(input(" Agora, insira a altura do rentângulo:  "))
+
+área = base*altura
+
+print(f"A área do seu rentangulo é {área}!")
